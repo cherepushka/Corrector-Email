b52efab42335f8c9c07b429ee6f71aaec11d6c3b
Misc1170 <sloopy1170@gmail.com>
2023-06-23 10:18:24 +0300

    edit Head Reg Exp

--- scripts/corrector.php
+++ scripts/corrector.php
@@ -1,37 +1,29 @@
-<link rel="stylesheet" href="/style/output.css">
-
+<link rel="stylesheet" href="../style/output.css">
 <?php
+require_once('../converter/idna_convert.class.php');
+define('InputPath', "../input/");
+define('OutputPath', "../output/");
 
-define('ROOT', dirname(__DIR__));
-define('InputPath', ROOT . "/input");
-define('OutputPath', ROOT . "/output");
-
-require_once(ROOT . '/converter/idna_convert.class.php');
 
 $fileName = $_GET['fileName'] ?? null;
 $checkAll = $_GET['checkAll'] ?? null;
-
 if ($checkAll) {
     $checkAll = $_GET['checkAll'] === 'all_files' ? true : false;
 }
 
 $csv = '';
-
 if (isset($fileName) || $checkAll) {
     // Считывание файла и запись в массив
     if ($checkAll) {
         $scanedFiles = scandir(InputPath);
-
         for ($i = 2; $i < count($scanedFiles); $i++) {
-            $csv .= file_get_contents(InputPath . "/" . $scanedFiles[$i]);
+            $csv .= file_get_contents(InputPath . $scanedFiles[$i]);
         }
     } else {
-        $csv = file_get_contents(InputPath . "/" . $fileName);
+        $csv = file_get_contents(InputPath . $fileName);
     }
-
     $rows = explode(PHP_EOL, $csv);
     $data = [];
-
     foreach ($rows as $row) {
         if ($row != null) {
             $data[] = trim($row, '\?,\.\SP\!\"\#\$\%\&\(\)\*\+\`\~\,\-\;\:\<\>\=\@');
@@ -49,78 +41,30 @@ if (isset($fileName) || $checkAll) {
     // Проверяет на окончание ru/com/org, разделяет строку на отдельные email и сравнивает их, удаляет если повторяется
     function filtration($str, $pattern)
     {
-        // if (in_array($str, ['buxgalteria2019@mail.ruludmilab2015@mail.ru', 'andrey@akvilon-holod.rularisa@akvilon-holod'])) {
-            preg_match_all($pattern, $str, $match);
-
-            // $foundResultFirst = implode("\n", $match[0]);
-            $foundResultFirst = $match[0];
-
-            foreach ($match[0] as $matched) {
-                $str = str_replace($matched, '', $str);
-            }
-
-            $notEntered = $str;
-        
-            return [
-                'str' => $str,
-                'initial_str_only_show' => $str,
-                'after_reg_1' => $foundResultFirst ?? false,
-                'after_reg_2' => false,
-                'balance_after_cut_2' => false,
-                'not_repeat_but_did_not_pass_reg' => false,
-                'did_not_pass_first_reg' => $notEntered ?? false,
-                'temp_arr' => false,
-            ];
-        // }
-        
-        $str_only_show = $str;
         if (stripos($str, ':')) {
             $pos = stripos($str, ':') + 1;
             $str = substr($str, $pos);
         }
-        $str = str_replace(['@@', '\'', '?', '/'], ['@', '', '', ''], $str);
-        $booleanResult = preg_match($pattern, $str, $found);
-        $comparisonPattern = '/^[a-zA-Z0-9_.+-?\']+@/i';
-        if ($booleanResult) {
-            $foundResultFirst = $found[0];
-            $str = trim(str_replace($foundResultFirst, '', $str), '\.');
-            $booleanResult = preg_match($pattern, $str, $found);
-
-            preg_match($comparisonPattern, $foundResultFirst, $firstCompar);
-            preg_match($comparisonPattern, $str, $secondCompar);
-            if ($firstCompar === $secondCompar) {
-                $str = str_replace($str, '', $str);
-            } else {
-                if ($booleanResult) {
-                    $foundResultSecond = $found[0];
-                    $str = str_replace($foundResultSecond, '', $str);
-                } else {
-                    if (!strpos('@', $str)) {
-                        $str = str_replace($str, '', $str);
-                    } else {
-                        $notRepeat = $str;
-                    }
-                }
-            }
-        } else {
-            $notEntered = $str;
+        if (stripos($str, '=')) {
+            $pos = stripos($str, '=') + 1;
+            $str = substr($str, $pos);
         }
-        // Выбирает Email которых осталось после первой проверки больше 1го
-        // $patterns = '/[a-zA-Z0-9_.+-?\']+@([a-z0-9.-]+)[.]((ru)|(com)|(net)|(org)|(kz)|(su))/i';
-        // $patterns = '/([\w+|_?|\-?]\.?+)+\@([\w+|\.?|\-?]+)\.(ru|com|net|org|kz|su)/i';
+        preg_match_all($pattern, $str, $match);
 
-        if (preg_match_all($pattern, $str, $found)) {
-            $tempArrStr = $found[0];
+        // $foundResultFirst = $match[0];
+        foreach ($match[0] as $matched) {
+            $foundResultFirst[] = trim($matched, '\.\_');
+
+            $str = str_replace($matched, '', $str);
+            $str = trim($matched, '\.\,');
         }
+
+        $notEntered = $str;
+
         return [
             'str' => $str,
-            'initial_str_only_show' => $str_only_show,
             'after_reg_1' => $foundResultFirst ?? false,
-            'after_reg_2' => $foundResultSecond ?? false,
-            'balance_after_cut_2' => $ostatokSecondReplace ?? false,
-            'not_repeat_but_did_not_pass_reg' => $notRepeat ?? false,
             'did_not_pass_first_reg' => $notEntered ?? false,
-            'temp_arr' => $temp_arr ?? false,
         ];
     }
 
@@ -128,108 +72,107 @@ if (isset($fileName) || $checkAll) {
     $correctEmail = [];
     $wrongEmail = [];
     $notEmptyString = [];
-    $tempArrStr = [];
 
     // Проверка всех Email первый preg_match
-    $patterns = '/([\w+|_?|\-?]\.?+)+\@([\w+|\.?|\-?]+)\.(ru|com|net|org|kz|su)/i';
+    $patterns = '/[\w+_\.\+\-\?\']+@([\w+.-]+?)\.(ru|com|net|org|kz|su)/i';
+    // $patterns = '/([\w+|_?|\-?]+\.?+)\@([\w+|\.?|\-?]+)+\.(ru|com|net|org|kz|su)/i'; Мишин вчерашний вечером
+    // $patterns = '/([\w+|_?|\-?]\.?+)+\@([\w+|\.?|\-?]+)\.(ru|com|net|org|kz|su)/i'; Изначальный вариант
     foreach ($data as $str) {
         $str = str_replace(['@@', '\'', '?', '/'], ['@', '', '', ''], $str);
         $arrayAfterRegCycle[] = filtration($str, $patterns);
     }
 
-    // Распределение по массивам корректных и некорректных email'ов после первого рег
-    
+    // Распределение по массивам корректных и некорректных email'ов после первого preg_match
     foreach ($arrayAfterRegCycle as $value) {
+
         if ($value['after_reg_1'] != null) {
             foreach ($value['after_reg_1'] as $reg) {
                 $correctEmail[] = $reg;
             }
         }
-        if ($value['after_reg_2'] != null) {
-            $correctEmail[] = $value['after_reg_2'];
-        }
         if ($value['did_not_pass_first_reg'] != null) {
             $wrongEmail[] = $value['did_not_pass_first_reg'];
         }
-        if ($value['not_repeat_but_did_not_pass_reg'] != null) {
-            $wrongEmail[] = $value['not_repeat_but_did_not_pass_reg'];
-        }
         if ($value['str'] != null) {
             $notEmptyString[] = $value['str'];
-        }
-        if ($value['temp_arr'] != null) {
-            foreach ($value['temp_arr'] as $item) {
-                $correctEmail[] = $item;
-            }
         }
     }
 
     // Добавление доменов в конец
     foreach ($wrongEmail as $key => &$email) {
-        
+        $email = trim($email, '\.\,');
+        if (!preg_match_all('#@#', $email)) {
+            unset($wrongEmail[$key]);
+            continue;
+        }
+
+        preg_match_all('#\@([\w+|\.?|\-?]+.?)#', $email, $match2);
+        switch ($match2[1]) {
+            default:
+            case "ya":
+            case "yand": {
+                    $email = str_replace('yand', 'yandex', $email);
+                    break;
+                }
+                break;
+        }
+        preg_match('#\@(yandex|mail|laserservice|akvilon-holod|gazprom-neft)#', $email, $rusDomain);
+        if (!empty($rusDomain[1])) {
+            $lengthDomain = strlen($rusDomain[1]);
+            $pos = strpos($email, $rusDomain[1]) + $lengthDomain;
+            $email = substr($email, 0, $pos) . '.ru';
+            $correctEmail[] = $email;
+            unset($wrongEmail[$key]);
+        }
+
         preg_match_all('#[\w+|_?|\-?]+\.?+\@([\w+|\.?|\-?]+)\.(\w+)?#', $email, $emailMached);
-
-        
-
         foreach ($emailMached[0] as $em) {
             preg_match_all('#\@([\w+|\.?|\-?]+)\.(\w+)?#', $em, $match);
 
             $continue = false;
-            for ($i = 0; $i < count($match[0]); $i++) {
-                switch ($match[1][$i]) {
 
-                    default : {
-                        switch ($match[2][$i]) {
-                            case "r" : {
-                                $replace = str_replace($match[0], '@' . $match[1][$i] . '.ru', $em);
-                                array_push($correctEmail, $replace);
-                                unset($wrongEmail[$key]);
-                                $continue = true;
-                                break;
+            for ($i = 0; $i < count($match[0]); $i++) {
+
+                switch ($match[1][$i]) {
+                    default: {
+                            switch ($match[2][$i]) {
+                                case "u":
+                                case "r": {
+                                        $replace = str_replace($match[0], '@' . $match[1][$i] . '.ru', $em);
+                                        array_push($correctEmail, $replace);
+                                        unset($wrongEmail[$key]);
+                                        $continue = true;
+                                        break;
+                                    }
+                                case "c":
+                                case "co": {
+                                        $replace = str_replace($match[0], '@' . $match[1][$i] . '.com', $em);
+                                        array_push($correctEmail, $replace);
+                                        $continue = true;
+                                        unset($wrongEmail[$key]);
+                                        break;
+                                    }
                             }
-                            case "c" :
-                            case "co" : {
-                                $replace = str_replace($match[0], '@' . $match[1][$i] . '.com', $em);
-                                array_push($correctEmail, $replace);
-                                $continue = true;
-                                unset($wrongEmail[$key]);
-                                break;
-                            }
+                            break;
                         }
-                        break;
-                    }
                 }
             }
             if ($continue) {
                 continue;
             }
         }
-        
-        // if (strripos($email, '@mail.')) {
-        //     $pos = strrpos($email, '.');
-        //     $email = substr($email, 0, $pos) . '.ru';
-        //     $correctEmail[] = $email;
-        //     unset($wrongEmail[$key]);
-        // }
-        // if (strripos($email, '@yandex')) {
-        //     $pos = strrpos($email, '.');
-        //     $email = substr($email, 0, $pos) . '.ru';
-        //     $correctEmail[] = $email;
-        //     unset($wrongEmail[$key]);
-        // }
-        // if (strripos($email, '@gmail')) {
-        //     $pos = strrpos($email, '.');
-        //     $email = substr($email, 0, $pos) . '.com';
-        //     $correctEmail[] = $email;
-        //     unset($wrongEmail[$key]);
-        // } else if (preg_match('/[.][rucm]$/', $email)) {
-        //     $email = preg_replace(['/[.][ru]$/', '/[.][cm]$/'], ['.ru', '.com'], $email);
-        //     $correctEmail[] = $email;
-        //     unset($wrongEmail[$key]);
-        // }
     }
-    
-    
+
+    $wrongEmail = array_unique($wrongEmail);
+    foreach ($wrongEmail as $key => &$item) {
+        foreach ($correctEmail as $corEmail) {
+            $pos = strpos($corEmail, $item);
+            if ($pos !== false) {
+                unset($wrongEmail[$key]);
+            }
+        }
+    }
+
     // Очищение массива с неправильными Email от пустых элементов и перекодировка Punycode в домен РФ
     $wrongEmails = [];
     $idn = new idna_convert();
@@ -244,10 +187,26 @@ if (isset($fileName) || $checkAll) {
         }
     }
 
-    $basename = basename($fileName, '.csv'); // Повторный вызов функции
+    $wrongEmails = array_unique($wrongEmails);
 
-    file_put_contents(OutputPath . "/" . $basename . '_correct_emails.csv', implode("\n", $correctEmail));
-    file_put_contents(OutputPath . "/" . $basename . '_incorrect_emails.csv', implode("\n", $wrongEmails));
+    if (isset($fileName)) {
+        $basename = basename($fileName, '.csv'); // Повторный вызов функции
+    }
+
+    // Запись в файл корректных Email
+    if (isset($fileName)) {
+        file_put_contents(OutputPath . $basename . '_correct_emails.csv', chr(0xEF) . chr(0xBB) . chr(0xBF));
+        file_put_contents(OutputPath . $basename . '_correct_emails.csv', implode("\n", array_unique($correctEmail)));
+    } else {
+        file_put_contents(OutputPath . 'all_files_correct_emails.csv', chr(0xEF) . chr(0xBB) . chr(0xBF));
+        file_put_contents(OutputPath . 'all_files_correct_emails.csv', implode("\n", array_unique($correctEmail)));
+    }
+    // Запись в файл ошибочных Email
+    if (isset($fileName)) {
+        file_put_contents(OutputPath . $basename . '_incorrect_emails.csv', implode("\n", $wrongEmails));
+    } else {
+        file_put_contents(OutputPath . 'all_files_incorrect_emails.csv', implode("\n", $wrongEmails));
+    }
 ?>
     <a class="px-10 py-4 cursor-pointer flex items-center" onclick="javascript:history.back(); return false;">
         <img src="../left_arrow.svg" alt=""><span class="text-red-400">Вернуться на главную</span>
@@ -257,22 +216,24 @@ if (isset($fileName) || $checkAll) {
         <div class="border-orange-400 bg-amber-300 rounded-md border-2 px-2 py-3 gap-4 flex flex-col w-1/3 my-4">
             <!-- Скачать Правильные Email -->
             <?php if (isset($checkAll)) { ?>
-                <a class="border-2 p-3 border-orange-400" href="/output/all_files_correct_emails.csv" download>
+                <a class="border-2 p-3 border-orange-400" href="../output/all_files_correct_emails.csv" download>
                     Скачать файл с <b>корректными</b> Email
                 </a>
             <?php } else { ?>
-                <a class="border-2 p-3 border-orange-400" href="/output/<?php echo basename($fileName, '.csv') ?>_correct_emails.csv" download>
+                <a class="border-2 p-3 border-orange-400" href="<?php echo OutputPath;
+                                                                echo basename($fileName, '.csv') ?>_correct_emails.csv" download>
                     Скачать файл с <b>корректными</b> Email
                 </a>
             <?php } ?>
 
             <!-- Скачать Неправильные Email -->
             <?php if (isset($checkAll)) { ?>
-                <a class="border-2 p-3 border-orange-400" href="/output/all_files_incorrect_emails.csv" download>
+                <a class="border-2 p-3 border-orange-400" href="../output/all_files_incorrect_emails.csv" download>
                     Скачать файл с <b>неправильными</b> Email
                 </a>
             <?php } else { ?>
-                <a class="border-2 p-3 border-orange-400" href="/output/<?php echo basename($fileName, '.csv') ?>_incorrect_emails.csv" download>
+                <a class="border-2 p-3 border-orange-400" href="<?php echo OutputPath;
+                                                                echo basename($fileName, '.csv') ?>_incorrect_emails.csv" download>
                     Скачать файл с <b>неправильными</b> Email
                 </a>
             <?php } ?>
